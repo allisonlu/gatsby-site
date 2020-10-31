98d8942ecea1997902da723d61b4bcdd5b810faa
Allison Lu <allisonlu820@gmail.com>
2020-10-31 00:53:13 -0700

    successfully installed and using sass!

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,26 @@
 import React from "react"
+import { Link } from "gatsby"
+import "./layout.scss"
+
+const NavLink = props => (
+    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+        <Link to={props.to}> {props.children} </Link>
+    </li>
+)
 
 export default function Layout ({ children }) {
     return (
-        <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 16px` }}>
+        <div>
+            <header>
+                <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+                    <h1>Page h1 title</h1>
+                </Link>
+                <ul style={{ listStyle: `none`, float: `right` }}>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about/">About</NavLink>
+                    <NavLink to="/contact/">Contact</NavLink>
+                </ul>
+            </header>
             {children}
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 export default function Home() {
   return (
     <Layout>
-      <h1>What a world.</h1>
-      <p>
-        <Link to="/about/">About</Link>
-      </p>
-      <p>
-        <Link to="/contact/">Contact</Link>
-      </p>
+      <h2>h2 header</h2>
+      <p>looooorem ipsum</p>
     </Layout>
   )
 }
